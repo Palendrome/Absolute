fc6174afcde196205c622af68348a1a0162dae07
Paul <spstudio@iafrica.com>
2018-01-28 15:42:21 +0200

    Working on mixins

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,3 @@
-var gulp = require('gulp'),
-watch = require('gulp-watch');
-
-gulp.task('default',function(){
-	console.log('hooray, you created a gulp task');
-});
-
-
-gulp.task('html', function(){
-	console.log("useful html function");
-});
-
-gulp.task('styles', function(){
-	console.log("watch the styles");
-});
-
-gulp.task('watch', function(){
-	
-	watch('./app/index.html', function() {		
-		gulp.start('html');
-	});	
-});
-
-watch('./app/assets/styles/**/*.css', function(){
-	
-	gulp.start('styles');
-});
+require('./gulp/tasks/styles');
+require('./gulp/tasks/watch');
 
